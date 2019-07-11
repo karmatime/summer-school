9702962f393429b3f2765e5980929758cd451164
GeorgescuEduard <47814670+GeorgescuEduard@users.noreply.github.com>
2019-07-11 14:32:29 +0300

    Add home

--- src/home.php
+++ src/home.php
@@ -2,6 +2,17 @@
 include "header.php";
 ?>
 
+<div class="container"  >
+                <div style="margin-top:150px; margin-left:30px; margin-right:30px;">
+                    <h1 >Open4Tech Summer School 2019</h1>
+                    <br>
+                    <p>Departamentul de Informatică din cadrul Facultății de Științe, Universitatea din Craiova și Asociația "Societatea pentru Tehnologii Computaționale - Society of Computer Technologies" împreună cu companiile Caphyon, NetRom Software și PRO BST IT organizează școala de vară <b>Open4Tech - Summer School 2019.</b></p>
+                    <p>Cursurile se vor desfășura în perioada <b>24 iunie 2019 - 12 iulie 2019</b>, în sala<b> 219</b>, Clădirea centrală, Universitatea din Craiova și sunt adresate studenților si elevilor pasionați de informatică.</p>
+                    <br>
+                   <p class="ml-5"> <a href="courses.php" class="btn mr-2">Cursuri</a> sau <a href="register.php" class="btn ml-2">Inscriere</a> </p>
+                </div>
+            </div>
+
 <?php
 include "footer.php";
 ?>
